7c02aec9d7205c3b756eb73df75c38bbfce71a32
afozbek <furkanozbek1995@gmail.com>
2020-12-26 14:32:11 +0300

    1 - expanding-cards - scripts

--- 1 - Expanding Cards/script.js
+++ 1 - Expanding Cards/script.js
@@ -1,1 +1,16 @@
 console.log('Hello From Script');
+
+const panelList = document.querySelectorAll(".panel");
+
+panelList.forEach(panel => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active")
+  })
+})
+
+function removeActiveClasses() {
+  panelList.forEach(panel => {
+    panel.classList.remove("active")
+  })
+}
